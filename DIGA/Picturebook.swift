6ec5cd003eb80170fab3f0cb0229257e5db603e9
kimurarinntaro <kimura_rintaro@mail.toyota.co.jp>
2022-07-01 08:49:11 +0900

    day:0630

---
+++ DIGA/Picturebook.swift
@@ -0,0 +1,18 @@
+//
+//  Picturebook.swift
+//  DIGA
+//
+//  Created by Rintaro Kimura on 2022/06/30.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class Picturebook:UIViewController{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+}
