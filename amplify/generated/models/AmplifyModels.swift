7add5697f3a65ab971822457a7d42f4b8dc58a1f
yuta-shoji <shoshoshon.music@gmail.com>
2022-07-04 08:42:39 +0900

    feat: merged from third

--- amplify/generated/models/AmplifyModels.swift
+++ amplify/generated/models/AmplifyModels.swift
@@ -5,7 +5,7 @@ import Foundation
 // Contains the set of classes that conforms to the `Model` protocol. 
 
 final public class AmplifyModels: AmplifyModelRegistration {
-  public let version: String = "7bbd3eb82228fc26056aeea5d52a1bfc"
+  public let version: String = "4850401dc78bea55666def3f1128888d"
   
   public func registerModels(registry: ModelRegistry.Type) {
     ModelRegistry.register(modelType: Character.self)
--- amplify/generated/models/Character+Schema.swift
+++ amplify/generated/models/Character+Schema.swift
@@ -10,6 +10,7 @@ extension Character {
     case rarity
     case first_met_place
     case met_count
+    case meet_stauts
     case createdAt
     case updatedAt
   }
@@ -28,6 +29,7 @@ extension Character {
       .field(character.rarity, is: .required, ofType: .int),
       .field(character.first_met_place, is: .required, ofType: .string),
       .field(character.met_count, is: .required, ofType: .int),
+      .field(character.meet_stauts, is: .required, ofType: .bool),
       .field(character.createdAt, is: .optional, isReadOnly: true, ofType: .dateTime),
       .field(character.updatedAt, is: .optional, isReadOnly: true, ofType: .dateTime)
     )
