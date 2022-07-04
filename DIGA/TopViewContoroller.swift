9316659e7fccf1adbab066722fa139b80ef3653c
yuta-shoji <shoshoshon.music@gmail.com>
2022-07-04 10:39:00 +0900

    fix: volume 0

--- DIGA/TopViewContoroller.swift
+++ DIGA/TopViewContoroller.swift
@@ -64,7 +64,7 @@ class TopViewController: UIViewController {
                 avPlayer.enableRate = true
                 avPlayer.rate = 1.0
                 avPlayer.numberOfLoops = -1
-                avPlayer.volume = 0.2
+                avPlayer.volume = 0
                 avPlayer.play()
             } catch {
                 print("error", error)
